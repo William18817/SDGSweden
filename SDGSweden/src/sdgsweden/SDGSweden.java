22e5d2b2c95125f3e7a18a07a5e7aa40120a73ed
Kalkilm <wilhelm.isflak@gmail.com>
2025-05-12 13:56:33 +0200

    la till form

--- SDGSweden/src/sdgsweden/SDGSweden.java
+++ SDGSweden/src/sdgsweden/SDGSweden.java
@@ -4,17 +4,27 @@
  */
 package sdgsweden;
 
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 /**
  *
  * @author willi
  */
 public class SDGSweden {
-
+    private static InfDB idb;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            idb = new InfDB("sdgsweden", "3306", "dbAdmin2024", "dbAdmin2024PW");
+            new Inloggning(idb).setVisible(true);
+            System.out.println("funkar");
+        } catch (InfException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
     
 }
